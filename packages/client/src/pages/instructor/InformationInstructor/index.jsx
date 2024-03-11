2b0412d8498cee3e54a084a64c31a6c28eb1cb70
HieuNguyen67 <hieu012846@gmail.com>
2024-03-11 23:27:16 +0700

    fix path url

--- packages/client/src/pages/instructor/InformationInstructor/index.jsx
+++ packages/client/src/pages/instructor/InformationInstructor/index.jsx
@@ -74,7 +74,7 @@ const InformationInstructor = () => {
                     >
                       Lưu thay đổi
                     </Button>
-                    <Link to="instructor/change-password">
+                    <Link to="/instructor/change-password">
                       <Button variant="text">Thay đổi mật khẩu</Button>
                     </Link>
                   </Grid>
