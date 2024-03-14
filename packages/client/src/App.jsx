e7d2ab808d427e4dd1c57f0a5c3ba4188c88cc8d
HieuNguyen67 <hieu012846@gmail.com>
2024-03-14 21:54:17 +0700

    add: role url

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "@components/ScrollToTop";
 import CssBaseline from "@mui/material/CssBaseline";
 import Toast from "@components/toast";
-import Home from "./pages/home";
 import MainLayout from "@components/layout";
 import { UserContextProvider } from "@contexts/user";
-import AdminRoutes from "@routes/admin-routes";
-import StudentRoutes from "@routes/student-routes";
-import InstructorRoutes from "@routes/instructor-routes";
+
+import React, { useContext } from "react";
+
+import Apps from "./components/App";
 function App() {
+ 
   return (
     <React.Fragment>
       <BrowserRouter>
@@ -18,12 +18,7 @@ function App() {
           <ScrollToTop />
           <Toast />
           <MainLayout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/admin/*" element={<AdminRoutes />} />
-              <Route path="/instructor/*" element={<InstructorRoutes />} />
-              <Route path="/student/*" element={<StudentRoutes />} />
-            </Routes>
+           <Apps/>
           </MainLayout>
         </UserContextProvider>
       </BrowserRouter>
